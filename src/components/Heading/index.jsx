4b306dfaa97840086dd5ec772a07a9bf3063620f
Rodrigo Silva <seu_email@email.com>
2023-11-02 08:42:43 -0300

    Initial

--- src/components/Heading/index.jsx
+++
@@ -1,7 +0,0 @@
-import { useCounterContext } from "../../contexts/CounterContext";
-
-export const Heading = () => {
-  const [state] = useCounterContext();
-
-  return <h1 style={{ fontSize: "60px" }}>{state.counter}</h1>;
-};
